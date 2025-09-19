06aebfbe553bdb45c248467414b08a911b2484ce
duongnoob23 <lamtiendung11082002@gmail.com>
2025-09-20 01:34:42 +0700

    fix Course api

--- backend/src/client/routes/courseClientRoutes.js
+++ backend/src/client/routes/courseClientRoutes.js
@@ -1,28 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/courseClientController");
-/**
- * Courses – client
- * - GET  /categories                (danh mục)
- * - GET  /levels                    (trình độ)
- * - GET  /instructors               (giảng viên)
- * - GET  /                          (list courses + filter: category_id, level_id, q)
- * - GET  /:course_id                (chi tiết course + details)
- * - GET  /:course_id/curriculum     (modules + lessons)
- */
 
-router.get("/categories", controller.getCourseByCategory);
+router.get("/categories", controller.getCategories);
 
-router.get("/levels", controller.getCoursebyLevel);
+router.get("/levels", controller.getLevels);
 
-router.get("/instructors", controller.getCourseByInstructor);
+router.get("/instructors", controller.getInstructors);
 
 router.get("/", controller.getCourse);
 
 router.get("/:course_id", controller.getCourseById);
 
-router.get("/:course_id/curriculum", (req, res) =>
-  res.status(501).json({ message: "Not implemented" })
-);
+router.get("/:course_id/curriculum", controller.getCourseCurriculum);
 
 module.exports = router;
