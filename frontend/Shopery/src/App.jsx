02ab78937334f23553e506161297d91302f5a3ff
dakacia <vucongduy201@gmail.com>
2025-09-20 00:03:50 +0700

    fix

--- frontend/Shopery/src/App.jsx
+++ frontend/Shopery/src/App.jsx
@@ -11,8 +11,9 @@ import { store } from "./redux/store";
 import "./App.css";
 import "./assets/styles/global.css";
 import "./assets/styles/reset.css";
+import Login from "./Client/pages/Auth/Login/Login";
+import AppRoutes from "./routes/routes";
 import Flashcard from "./Client/pages/Flashcard/Flashcard";
-
 function App() {
   return (
     <Provider store={store}>
@@ -20,6 +21,7 @@ function App() {
         <Router>
           <div className="App">
             {/* <AppRoutes /> */}
+            {/* <Login /> */}
             <Flashcard />
           </div>
         </Router>
